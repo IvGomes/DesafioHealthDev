4e72c1ecfdc681169a96f8de99d62ec6f1c01ae8
IvGomes <ivan.kyogomes@gmail.com>
2023-01-08 21:50:50 -0300

    adjustment on package.json

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,32 @@
-import express, {Request, Response, NextFunction} from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import "express-async-errors";
-import { routes } from './routes';
-
+import express, { Request, Response, NextFunction } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import 'express-async-errors'
+import { routes } from './routes'
 
 dotenv.config()
 
-const app = express();
-
+const app = express()
 
 // Middlewares...
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-    if(err instanceof Error) {
-        return response.status(400).json({
-            message: err.message
-        })
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      })
     }
 
     return response.status(500).json({
-        status: "error",
-        message: "Internal server error"
+      status: 'error',
+      message: 'Internal server error',
     })
-})
+  },
+)
 
-
-app.listen(process.env.REACT_APP_PORT, () => console.log("🔥 server is running..."))
+app.listen(process.env.REACT_APP_PORT, () =>
+  console.log(`🔥 server is running on port ${process.env.REACT_APP_PORT}...`),
+)
