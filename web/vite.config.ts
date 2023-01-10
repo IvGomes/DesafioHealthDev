46952244a0162d5e740f598fd1c1820c2a7e90ee
IvGomes <ivan.kyogomes@gmail.com>
2023-01-10 10:22:54 -0300

    restore config vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        login: resolve(__dirname, 'src/pages/SigninupPage.tsx'),
-      },
-    },
-  }
-
 })
 
 
