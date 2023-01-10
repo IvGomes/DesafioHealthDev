9c6eef1c1bd01f98f5d5d794f0bf032e1b6e8f96
IvGomes <ivan.kyogomes@gmail.com>
2023-01-09 22:44:04 -0300

    remove unique paramns from cols db

---
+++ server/prisma/migrations/20230110014306_remove_unique_from_cols/migration.sql
@@ -0,0 +1,17 @@
+-- DropIndex
+DROP INDEX "generalDatas_cns_key";
+
+-- DropIndex
+DROP INDEX "generalDatas_cpf_key";
+
+-- DropIndex
+DROP INDEX "generalDatas_email_key";
+
+-- DropIndex
+DROP INDEX "generalDatas_phone_key";
+
+-- DropIndex
+DROP INDEX "generalDatas_rg_key";
+
+-- DropIndex
+DROP INDEX "professionalDatas_numeroConselho_key";
